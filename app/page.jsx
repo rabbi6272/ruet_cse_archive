093daa54c38ec2d55ddeeb9c0ed8feda90944c4a
𝙱𝚒𝚝𝚝𝚘 𝚂𝚊𝚑𝚊 <i.bittosaha@gmail.com>
2025-07-13 23:21:36 +0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,17 @@ import { FeaturesList } from "@/components/home-page/features_list";
 export default function Home() {
   return (
     <div className="min-h-screen w-full">
-<h1 className="text-5xl font-bold text-gray-900">
+
+      <div className = "flex flex-col mx-auto px-4 sm:px-6 lg:px-8">
+      <h1 className="text-5xl font-bold text-gray-900">
           Welcome to CSE  Archive!
         </h1>
         <p className="mt-4 text-lg text-gray-600">
           This is a demo of a responsive navbar with morphic design using
           Tailwind CSS.
         </p>
+      </div>
+
       
       {/* Slideshow */}
       <Slideshow />
