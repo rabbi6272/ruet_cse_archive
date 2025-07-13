1aff2b2024bf0cea51ac4b49b22c519d4dda5728
𝙱𝚒𝚝𝚝𝚘 𝚂𝚊𝚑𝚊 <i.bittosaha@gmail.com>
2025-07-13 23:26:39 +0600

    Update features_list.jsx

--- components/home-page/features_list.jsx
+++ components/home-page/features_list.jsx
@@ -4,6 +4,9 @@ export function FeaturesList() {
 
     return (<>
     
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">world’s potential</span></h1>
+<p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+<br/>
     <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
     <li className="flex items-center space-x-3 rtl:space-x-reverse">
         <svg className="shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
