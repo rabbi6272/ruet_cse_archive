ee05d3ba26fb653cda9f4714ad42204bb4487428
𝙱𝚒𝚝𝚝𝚘 𝚂𝚊𝚑𝚊 <i.bittosaha@gmail.com>
2025-07-13 23:43:37 +0600

    Update features_list.jsx

--- components/home-page/features_list.jsx
+++ components/home-page/features_list.jsx
@@ -41,7 +41,7 @@ export function FeaturesList() {
 </ul>
         <br/>
         <br/>
-<h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+<h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-dark md:text-5xl lg:text-6xl">
   <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
     Better Data
   </span> Scalable AI.
@@ -216,6 +216,9 @@ export function FeaturesList() {
     </div>
   </div>
 </div>
+
+    </br>
+            </br>
      
 
     
