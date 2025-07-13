10f2217646c6f8f8d5fb0c743c6513ef4ea1de55
𝙱𝚒𝚝𝚝𝚘 𝚂𝚊𝚑𝚊 <i.bittosaha@gmail.com>
2025-07-13 22:41:59 +0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -9,12 +9,15 @@ export default function Home() {
       {/* <!-- Main content --> */}
       <div className="w-full h-full flex flex-col mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="text-5xl font-bold text-gray-900">
-          Welcome to our website
+          Welcome to CSE  Archive!
         </h1>
         <p className="mt-4 text-lg text-gray-600">
           This is a demo of a responsive navbar with morphic design using
           Tailwind CSS.
         </p>
+
+        <br/>
+        <br/>
       </div>
     </div>
   );
