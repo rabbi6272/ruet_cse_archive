3399b69fcff0e9130ff8c17915ad1dab71b5f29d
𝙱𝚒𝚝𝚝𝚘 𝚂𝚊𝚑𝚊 <i.bittosaha@gmail.com>
2025-07-13 23:41:30 +0600

    Update features_list.jsx

--- components/home-page/features_list.jsx
+++ components/home-page/features_list.jsx
@@ -4,8 +4,8 @@ export function FeaturesList() {
 
     return (<>
     
-<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-dark">We invest in the <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">world’s potential</span></h1>
-<p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+<h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-dark">We invest in the <span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">world’s potential</span></h1>
+<p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
 <br/>
     <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
     <li className="flex items-center space-x-3 rtl:space-x-reverse">
@@ -41,7 +41,182 @@ export function FeaturesList() {
 </ul>
         <br/>
         <br/>
-        <br/>
+<h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+  <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+    Better Data
+  </span> Scalable AI.
+</h1>
+<p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+  Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
+</p>
+    <br/>
+
+        <div 
+  id="accordion-color" 
+  data-accordion="collapse" 
+  data-active-classes="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white"
+>
+  <h2 id="accordion-color-heading-1">
+    <button 
+      type="button" 
+      className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800 gap-3" 
+      data-accordion-target="#accordion-color-body-1" 
+      aria-expanded="true" 
+      aria-controls="accordion-color-body-1"
+    >
+      <span>What is Flowbite?</span>
+      <svg 
+        data-accordion-icon 
+        className="w-3 h-3 rotate-180 shrink-0" 
+        aria-hidden="true" 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 10 6"
+      >
+        <path 
+          stroke="currentColor" 
+          strokeLinecap="round" 
+          strokeLinejoin="round" 
+          strokeWidth="2" 
+          d="M9 5 5 1 1 5"
+        />
+      </svg>
+    </button>
+  </h2>
+  <div 
+    id="accordion-color-body-1" 
+    className="hidden" 
+    aria-labelledby="accordion-color-heading-1"
+  >
+    <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+      <p className="mb-2 text-gray-500 dark:text-gray-400">
+        Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
+      </p>
+      <p className="text-gray-500 dark:text-gray-400">
+        Check out this guide to learn how to{" "}
+        <a 
+          href="/docs/getting-started/introduction/" 
+          className="text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          get started
+        </a>{" "}
+        and start developing websites even faster with components on top of Tailwind CSS.
+      </p>
+    </div>
+  </div>
+
+  <h2 id="accordion-color-heading-2">
+    <button 
+      type="button" 
+      className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800 gap-3" 
+      data-accordion-target="#accordion-color-body-2" 
+      aria-expanded="false" 
+      aria-controls="accordion-color-body-2"
+    >
+      <span>Is there a Figma file available?</span>
+      <svg 
+        data-accordion-icon 
+        className="w-3 h-3 rotate-180 shrink-0" 
+        aria-hidden="true" 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 10 6"
+      >
+        <path 
+          stroke="currentColor" 
+          strokeLinecap="round" 
+          strokeLinejoin="round" 
+          strokeWidth="2" 
+          d="M9 5 5 1 1 5"
+        />
+      </svg>
+    </button>
+  </h2>
+  <div 
+    id="accordion-color-body-2" 
+    className="hidden" 
+    aria-labelledby="accordion-color-heading-2"
+  >
+    <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+      <p className="mb-2 text-gray-500 dark:text-gray-400">
+        Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.
+      </p>
+      <p className="text-gray-500 dark:text-gray-400">
+        Check out the{" "}
+        <a 
+          href="https://flowbite.com/figma/" 
+          className="text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          Figma design system
+        </a>{" "}
+        based on the utility classes from Tailwind CSS and components from Flowbite.
+      </p>
+    </div>
+  </div>
+
+  <h2 id="accordion-color-heading-3">
+    <button 
+      type="button" 
+      className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800 gap-3" 
+      data-accordion-target="#accordion-color-body-3" 
+      aria-expanded="false" 
+      aria-controls="accordion-color-body-3"
+    >
+      <span>What are the differences between Flowbite and Tailwind UI?</span>
+      <svg 
+        data-accordion-icon 
+        className="w-3 h-3 rotate-180 shrink-0" 
+        aria-hidden="true" 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 10 6"
+      >
+        <path 
+          stroke="currentColor" 
+          strokeLinecap="round" 
+          strokeLinejoin="round" 
+          strokeWidth="2" 
+          d="M9 5 5 1 1 5"
+        />
+      </svg>
+    </button>
+  </h2>
+  <div 
+    id="accordion-color-body-3" 
+    className="hidden" 
+    aria-labelledby="accordion-color-heading-3"
+  >
+    <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+      <p className="mb-2 text-gray-500 dark:text-gray-400">
+        The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.
+      </p>
+      <p className="mb-2 text-gray-500 dark:text-gray-400">
+        However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.
+      </p>
+      <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+      <ul className="ps-5 text-gray-500 list-disc dark:text-gray-400">
+        <li>
+          <a 
+            href="https://flowbite.com/pro/" 
+            className="text-blue-600 dark:text-blue-500 hover:underline"
+          >
+            Flowbite Pro
+          </a>
+        </li>
+        <li>
+          <a 
+            href="https://tailwindui.com/" 
+            rel="nofollow" 
+            className="text-blue-600 dark:text-blue-500 hover:underline"
+          >
+            Tailwind UI
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+     
 
     
     </>);
