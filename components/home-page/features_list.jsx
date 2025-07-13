49d8e2d79c4fc77be3338b06d49f847640e88c0b
𝙱𝚒𝚝𝚝𝚘 𝚂𝚊𝚑𝚊 <i.bittosaha@gmail.com>
2025-07-13 22:58:48 +0600

    Update features_list.jsx

--- components/home-page/features_list.jsx
+++ components/home-page/features_list.jsx
@@ -21,19 +21,19 @@ export function FeaturesList() {
         <svg className="shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
         </svg>
-        <span>Team size: <span className="font-semibold text-gray-900 dark:text-black">1 developer</span></span>
+        <span>Team size: <span className="font-semibold text-gray-900 dark:text-dark">1 developer</span></span>
     </li>
     <li className="flex items-center space-x-3 rtl:space-x-reverse">
         <svg className="shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
         </svg>
-        <span>Premium support: <span className="font-semibold text-gray-900 dark:text-black">6 months</span></span>
+        <span>Premium support: <span className="font-semibold text-gray-900 dark:text-dark">6 months</span></span>
     </li>
     <li className="flex items-center space-x-3 rtl:space-x-reverse">
         <svg className="shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
         </svg>
-        <span>Free updates: <span className="font-semibold text-gray-900 dark:text-black">6 months</span></span>
+        <span>Free updates: <span className="font-semibold text-gray-900 dark:text-dark">6 months</span></span>
     </li>
 </ul>
 
