8e50595cf8fcc6219444a7883b2e2ceb9059dcbf
𝙱𝚒𝚝𝚝𝚘 𝚂𝚊𝚑𝚊 <i.bittosaha@gmail.com>
2025-07-13 23:24:58 +0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -14,6 +14,7 @@ export default function Home() {
           This is a demo of a responsive navbar with morphic design using
           Tailwind CSS.
         </p>
+        <br/>
       </div>
 
       
